0ab1e5a0954aef96f10fa863146b4864dfe7102f
Benedict Romp <benedict.romp@googlemail.com>
2023-10-05 10:37:19 +0200

    Version increase

--- lib/services/HeartbeatService.ts
+++ lib/services/HeartbeatService.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import moment from "moment";
 import { ActivitiesService } from "./ActivitiesService";
 import { SystemService } from "./SystemService";
+import { ProcessService } from "./ProcessService";
 
 export class HeartbeatService {
     lastHeartbeat: any = null;
@@ -16,10 +17,13 @@ export class HeartbeatService {
         const activitiesService = new ActivitiesService();
         activitiesService.store("system.last_heartbeat", moment().format("YYYY-MM-DD HH:mm:ss"));
 
+        const processService = new ProcessService();
+
         try {
             await axios.post("https://monitoring-api.webtypen.de/api/heartbeat", {
                 data: data,
                 server: config.server,
+                processes: processService.processesStatus(),
             });
         } catch (e: any) {
             console.error(e);
